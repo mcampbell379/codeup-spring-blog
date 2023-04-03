fa53398877f52e25d282ec12b7e4238c59e6716a
mcampbell379 <campbell.mitchell45@gmail.com>
2023-04-03 09:45:20 -0500

    edited syntax

--- src/main/java/com/codeup/codeupspringblog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/codeupspringblog/services/UserDetailsLoader.java
@@ -10,15 +10,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsLoader implements UserDetailsService {
-    private final UserRepository users;
+    private final UserRepository userDao;
 
-    public UserDetailsLoader(UserRepository users) {
-        this.users = users;
+    public UserDetailsLoader(UserRepository userDao) {
+        this.userDao = userDao;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = users.findByUsername(username);
+        User user = userDao.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("No user found for " + username);
         }
